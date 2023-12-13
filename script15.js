13748afe26874361493c3afd84cba67600d1b019
Mudassir Raza <108223551+mudassir22@users.noreply.github.com>
2023-12-13 11:39:34 +0530

    Update script15.js

--- script15.js
+++ script15.js
@@ -227,87 +227,8 @@ function sendRequest() {
   .catch(error => console.error('Error fetching JSON:', error));
 
   */
-/*
+
   document.addEventListener("DOMContentLoaded", function () {
-  // Fetch JSON data
-  fetch("selection.json")
-    .then(response => response.json())
-    .then(data => {
-      const stateDropdown = document.getElementById("stateName");
-      const zoneDropdown = document.getElementById("zoneName");
-      const numberDropdown = document.getElementById("displayNumber");
-
-      // Populate States
-      data.states.forEach(state => {
-        const stateOption = document.createElement("option");
-        stateOption.value = state.value;
-        stateOption.textContent = state.label;
-        stateDropdown.appendChild(stateOption);
-      });
-
-      // Event listener for State change
-      stateDropdown.addEventListener("change", function () {
-        const selectedState = stateDropdown.value;
-        populateZones(selectedState);
-      });
-
-      // Event listener for Zone change
-      zoneDropdown.addEventListener("change", function () {
-        const selectedZone = zoneDropdown.value;
-        populateNumbers(selectedZone);
-      });
-    })
-    .catch(error => console.error("Error fetching JSON:", error));
-});
-
-function populateZones(selectedState) {
-  fetch("selecion.json")
-    .then(response => response.json())
-    .then(data => {
-      const zoneDropdown = document.getElementById("zoneName");
-
-      // Clear existing options
-      zoneDropdown.innerHTML = "<option value=''>Select Zone</option>";
-
-      // Populate Zones based on the selected State
-      const stateData = data.states.find(state => state.value === selectedState);
-      if (stateData && stateData.zones) {
-        stateData.zones.forEach(zone => {
-          const zoneOption = document.createElement("option");
-          zoneOption.value = zone.value;
-          zoneOption.textContent = zone.label;
-          zoneDropdown.appendChild(zoneOption);
-        });
-      }
-    })
-    .catch(error => console.error("Error fetching JSON:", error));
-}
-
-function populateNumbers(selectedZone) {
-  fetch("selection.json")
-    .then(response => response.json())
-    .then(data => {
-      const numberDropdown = document.getElementById("displayNumber");
-
-      // Clear existing options
-      numberDropdown.innerHTML = "<option value=''>Select Display Number</option>";
-
-      // Populate Numbers based on the selected Zone
-      const zoneData = data.zones.find(zone => zone.value === selectedZone);
-      if (zoneData && zoneData.numbers) {
-        zoneData.numbers.forEach(number => {
-          const numberOption = document.createElement("option");
-          numberOption.value = number.value;
-          numberOption.textContent = number.label;
-          numberDropdown.appendChild(numberOption);
-        });
-      }
-    })
-    .catch(error => console.error("Error fetching JSON:", error));
-}
-*/
-
-document.addEventListener("DOMContentLoaded", function () {
   // Fetch JSON data
   fetch("example.json")
     .then(response => response.json())
@@ -384,3 +305,4 @@ function populateNumbers(selectedZone) {
     })
     .catch(error => console.error("Error fetching JSON:", error));
 }
+
