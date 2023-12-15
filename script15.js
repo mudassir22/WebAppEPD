f9fa446796d1ada27f1d403debf0d6032ce1f917
Mudassir Raza <108223551+mudassir22@users.noreply.github.com>
2023-12-15 15:52:29 +0530

    Update script15.js

--- script15.js
+++ script15.js
@@ -47,7 +47,7 @@ function sendRequest() {
   }
 
   // Update the URL dynamically
-  const baseUrl = "https://a1ac-122-160-48-182.ngrok-free.app";
+  const baseUrl = "https://8ae2-122-160-48-182.ngrok-free.app";
   const dynamicUrl = `/${state}/POST/data-channel/${zone}/${displayNumber}`;
 
   if (computedStyle.display === 'block') {
